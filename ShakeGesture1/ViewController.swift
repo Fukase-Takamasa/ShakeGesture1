d1e4273959de47839478a94213ce3ed4f6d26560
Takamasa Fukase <takama224@yahoo.co.jp>
2020-01-31 00:25:12 +0900

    Initial Commit

---
+++ ShakeGesture1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShakeGesture1
+//
+//  Created by 深瀬貴将 on 2020/01/31.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShakeGesture1Tests/ShakeGesture1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ShakeGesture1Tests.swift
+//  ShakeGesture1Tests
+//
+//  Created by 深瀬貴将 on 2020/01/31.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import XCTest
+@testable import ShakeGesture1
+
+class ShakeGesture1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
