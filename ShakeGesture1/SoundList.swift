2407f5c275b9cd0e8839d67df9da87721a2e1e65
Takamasa Fukase <takama224@yahoo.co.jp>
2020-02-05 20:35:37 +0900

    音源名を別ファイルにまとめて格納し、それを参照するようにした。

---
+++ ShakeGesture1/SoundList.swift
@@ -0,0 +1,35 @@
+//
+//  SoundList.swift
+//  ShakeGesture1
+//
+//  Created by 深瀬貴将 on 2020/02/05.
+//  Copyright © 2020 fukase. All rights reserved.
+//
+
+import Foundation
+
+open class SoundList {
+    
+    static let sounds = [
+        [
+            "shake1": "katana_swing",
+            "shake2": "katana_slash",
+            "start": "katana_drawing",
+            "gyro": "katana_hold"
+        ],
+        
+        [
+            "shake1": "light_saber3",
+            "shake2": "light_saber3",
+            "start": "electric_chain",
+            "gyro": "katana_hold"
+        ],
+        
+        [
+            "shake1": "katana_swing",
+            "shake2": "katana_slash",
+            "start": "katana_drawing",
+            "gyro": "katana_hold"
+        ]
+    ]
+}
--- ShakeGesture1/ViewController.swift
+++ ShakeGesture1/ViewController.swift
@@ -14,9 +14,15 @@ import AudioToolbox
 class ViewController: UIViewController {
     
     let motionManager = CMMotionManager()
+    
     var shakeAudioPlayer = AVAudioPlayer()
+    var shakeAudioPlayer2 = AVAudioPlayer()
     var startAudioPlayer = AVAudioPlayer()
     var gyroAudioPlayer = AVAudioPlayer()
+    
+    var preBool = false
+    var postBool = false
+    var currentShakeSoundType = true
     
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var button: UIButton!
@@ -24,14 +30,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setSounds()
         getAccelerometer()
-        setShakeSound("katana_slash")
-        setStartSound("katana_drawing")
-        setGyroSound("katana_hold")
+        getGyro()
     }
     
     @IBAction func tapSegmentControl(_ sender: Any) {
-        //後でセグメントindexによって音源をセットし直す
+        setSounds()
     }
     
     @IBAction func tapButton(_ sender: Any) {
@@ -52,9 +57,6 @@ class ViewController: UIViewController {
     func updateAccelerationData(data: CMAcceleration) {
         print("加速度データ: \(data)")
         
-        var preBool = false
-        var postBool = false
-        
         let x = data.x
         let y = data.y
         let z = data.z
@@ -64,7 +66,7 @@ class ViewController: UIViewController {
             postBool = true
         }
         
-        if !postBool && synthetic >= 3 {
+        if !postBool && synthetic >= 4 {
             shakeAudioPlayer.currentTime = 0 //再生中の音を止める
             shakeAudioPlayer.play()
             
@@ -73,7 +75,7 @@ class ViewController: UIViewController {
             preBool = true
         }
 
-        if postBool && synthetic >= 3 {
+        if postBool && synthetic >= 4 {
             shakeAudioPlayer.currentTime = 0
             shakeAudioPlayer.play()
             
@@ -96,7 +98,6 @@ class ViewController: UIViewController {
     }
     
     func changeShakeSoundInCurrentMode(_ currentShakeSoundType: Bool) -> Bool {
-        let shakeSoundList = [["katana_swing", "katana_slash"], ["light_saber", "light_saber"], ["light_saber", "light_saber"]]
         let segmentIndex = self.segmentControl.selectedSegmentIndex
         if currentShakeSoundType {
             setGyroSound(shakeSoundList[segmentIndex][1])
@@ -109,21 +110,30 @@ class ViewController: UIViewController {
     
     func updateGyroData(data: CMRotationRate) {
         print("ジャイロデータ: \(data)")
-
-        var currentShakeSoundType = true
-        
         let y = data.y
         let synthetic = (y * y)
         
-        if synthetic >= 6 {
+        if synthetic >= 8 {
+            print("ジャイロセンサー反応")
             currentShakeSoundType = changeShakeSoundInCurrentMode(currentShakeSoundType)
+            shakeAudioPlayer.currentTime = 0
             gyroAudioPlayer.play()
+            print("currentShakeSoundType: \(self.currentShakeSoundType)")
         }
     }
 
 }
 
 extension ViewController: AVAudioPlayerDelegate {
+    
+    func setSounds() {
+        let index = self.segmentControl.selectedSegmentIndex
+        setShakeSound(SoundList.sounds[index]["shake1"]!)
+        setShakeSound2(SoundList.sounds[index]["shake2"]!)
+        setStartSound(SoundList.sounds[index]["start"]!)
+        setGyroSound(SoundList.sounds[index]["gyro"]!)
+    }
+    
     func setShakeSound(_ resourceFileName: String) {
         guard let path = Bundle.main.path(forResource: resourceFileName, ofType: "mp3") else {
             print("shake音声ファイルが見つかりません")
@@ -134,6 +144,21 @@ extension ViewController: AVAudioPlayerDelegate {
             
             shakeAudioPlayer.delegate = self
             shakeAudioPlayer.prepareToPlay()
+        } catch {
+            print("shake音声セットエラー")
+        }
+    }
+    
+    func setShakeSound2(_ resourceFileName: String) {
+        guard let path = Bundle.main.path(forResource: resourceFileName, ofType: "mp3") else {
+            print("shake音声ファイルが見つかりません")
+            return
+        }
+        do {
+            shakeAudioPlayer2 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+            shakeAudioPlayer2.delegate = self
+            shakeAudioPlayer2.prepareToPlay()
         } catch {
             print("shake音声セットエラー")
         }
@@ -156,16 +181,16 @@ extension ViewController: AVAudioPlayerDelegate {
     
     func setGyroSound(_ resourceFileName: String) {
         guard let path = Bundle.main.path(forResource: resourceFileName, ofType: "mp3") else {
-            print("start音声ファイルが見つかりません。")
+            print("gyro音声ファイルが見つかりません。")
             return
         }
         do {
-            startAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            gyroAudioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
             
-            startAudioPlayer.delegate = self
-            startAudioPlayer.prepareToPlay()
+            gyroAudioPlayer.delegate = self
+            gyroAudioPlayer.prepareToPlay()
         } catch {
-            print("start音声セットエラー")
+            print("gyro音声セットエラー")
         }
     }
 }
